834c96535fc34ba07fcc2aff483570545173697a
CodingTheories <CodingTheories@gmail.com>
2020-11-21 21:07:52 -0500

    fixing range slider and adding multiple filters

--- src/Dashboard.py
+++ src/Dashboard.py
@@ -4,11 +4,17 @@ from datetime import datetime
 import dash  # (version 1.12.0) pip install dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State, ALL
 
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from src.models import DataLoader
+import asyncio
+
+loader = DataLoader.DataLoader()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(loader.start())
 
 COLOR = ['rgb(31, 119, 180)']
 
@@ -29,12 +35,8 @@ app.layout = html.Div(children=[
         dcc.Tab(label='Real-Time Interactive Plot', children=[
 
             # This dropdown is going to loop through the data files, display the names and allow the user to select the meter
-            dcc.Dropdown(id="slct_meter",
-                         options=[{'label': i.split('_results')[0], 'value': i} for i in entries],
-                         multi=False,
-                         value=entries[0],
-                         style={'width': "40%"}
-                         ),
+            html.Button("Add Filter", id="add-filter", n_clicks=0),
+            html.Div(id='dropdown-container', children=[]),
             dcc.Checklist(
                 id='slct_predict',
                 options=[
@@ -105,6 +107,7 @@ app.layout = html.Div(children=[
     Output(component_id='uncg_graph2', component_property='figure'),
     [Input(component_id='slct_meter2', component_property='value')],
     [Input(component_id='slct_period2', component_property='value')],
+
 )
 def update_graph2(slct_meter2, slct_period2):
     df1 = pd.read_csv("../data/Analysis/" + slct_meter2, parse_dates=['Datetime'],
@@ -175,87 +178,121 @@ def update_graph2(slct_meter2, slct_period2):
 
 @app.callback(
     Output(component_id='uncg_graph', component_property='figure'),
+
+    Output(component_id='time_range', component_property='min'),
+    Output(component_id='time_range', component_property='max'),
+    Output(component_id='time_range', component_property='marks'),
+    Output(component_id='time_range', component_property='value'),
+
     [Input(component_id='slct_predict', component_property='value')],
-    [Input(component_id='slct_meter', component_property='value')],
     [Input(component_id='slct_period', component_property='value')],
     [Input(component_id='slct_consum', component_property='value')],
-    [Input(component_id='time_range', component_property='value')]
-)
-def update_graph(slct_predict, slct_meter, slct_period, slct_consum, time_range):
-    # ------------------------------------------------------------------------------
-    # Import and clean data (importing csv into pandas)
-    df = pd.read_csv("../data/Analysis/" + slct_meter, parse_dates=['Datetime'],
-                     date_parser=lambda col: pd.to_datetime(col, utc=True), index_col='Datetime')
+    Input({'type': 'slct_meter', 'index': ALL}, 'value'),
+    [State(component_id='time_range', component_property='value')]
 
-    start_time = df[df.index.year == time_range[0]]
+)
+def update_graph(slct_predict, slct_period, slct_consum, slct_meter, time_range):
+    df = {}
+    for i in range(len(slct_meter)):
+        df[i] = loader.load_file(slct_meter[i].split('.csv')[0])
+    # print(df[0])
+
+    start_time = df[0][df[0].index.year == time_range[0]]
     start_time = start_time.index.min()
 
-    end_time = df[df.index.year == time_range[1]]
+    end_time = df[0][df[0].index.year == time_range[1]]
     end_time = end_time.index.max()
 
-    if slct_consum == 'Total consumption':
-        dff = df.loc[start_time:end_time].resample(slct_period).sum()
-    elif slct_consum == 'Average consumption':
-        dff = df.loc[start_time:end_time].resample(slct_period).mean()
-    # temp
-    elif slct_consum == 'Hourly consumption':
-        dff = df.loc[start_time:end_time]
+
+    if(type(start_time) == pd._libs.tslibs.nattype.NaTType):
+        start_time = df[0].index.min()
+    print(start_time)
+    print(end_time)
+
 
     fig = go.Figure()
+    for i in range(len(df)):
+        if slct_consum == 'Total consumption':
+            dff = df[i].loc[start_time:end_time].resample(slct_period).sum()
+        elif slct_consum == 'Average consumption':
+            dff = df[i].loc[start_time:end_time].resample(slct_period).mean()
+        # temp
+        elif slct_consum == 'Hourly consumption':
+            dff = df[i].loc[start_time:end_time]
 
-    # Add traces
-    fig.add_trace(go.Scatter(
-        name='Actual',
-        x=dff.index,
-        y=dff['Actual'],
-        mode='lines',
-        line=dict(color=COLOR[0]),
-    ))
-    print(slct_predict)
-    # print(len(slct_predict))
-    if ('Include prediction' in slct_predict):
-        fig.add_trace(go.Scatter(x=dff.index, y=dff['Predicted'],
-                                 mode='lines',
-                                 name='Predicted'))
-    if ('Include CI' in slct_predict):
-        fig.add_traces([
-            go.Scatter(
-                name='Upper Bound',
-                x=dff.index,
-                y=dff['obs_ci_upper'],
-                mode='lines',
-                marker=dict(color="#444"),
-                line=dict(width=0),
-                showlegend=False
+        # Add traces
+        fig.add_trace(go.Scatter(
+            name='Actual',
+            x=dff.index,
+            y=dff['Actual'],
+            mode='lines',
+            line=dict(color=COLOR[0]),
+        ))
+        # print(slct_predict)
+        # print(len(slct_predict))
+        if ('Include prediction' in slct_predict):
+            fig.add_trace(go.Scatter(x=dff.index, y=dff['Predicted'],
+                                     mode='lines',
+                                     name='Predicted'))
+        if ('Include CI' in slct_predict):
+            fig.add_traces([
+                go.Scatter(
+                    name='Upper Bound',
+                    x=dff.index,
+                    y=dff['obs_ci_upper'],
+                    mode='lines',
+                    marker=dict(color="#444"),
+                    line=dict(width=0),
+                    showlegend=False
+                ),
+                go.Scatter(
+                    name='Lower Bound',
+                    x=dff.index,
+                    y=dff['obs_ci_lower'],
+                    marker=dict(color="#444"),
+                    line=dict(width=0),
+                    mode='lines',
+                    fillcolor='rgba(68, 68, 68, 0.3)',
+                    fill='tonexty',
+                    showlegend=False
+                )
+            ])
+
+        # fig = px.line(x=dff.index, y=dff['Actual'])
+
+        fig.update_layout(
+            hovermode="x",
+            yaxis=dict(
+                title_text=slct_consum,
             ),
-            go.Scatter(
-                name='Lower Bound',
-                x=dff.index,
-                y=dff['obs_ci_lower'],
-                marker=dict(color="#444"),
-                line=dict(width=0),
-                mode='lines',
-                fillcolor='rgba(68, 68, 68, 0.3)',
-                fill='tonexty',
-                showlegend=False
+            xaxis=dict(
+                title_text='Time',
             )
-        ])
-
-    # fig = px.line(x=dff.index, y=dff['Actual'])
-
-    fig.update_layout(
-        hovermode="x",
-        yaxis=dict(
-            title_text=slct_consum,
-        ),
-        xaxis=dict(
-            title_text='Time',
         )
-    )
+    min, max, marks, value =loader.x(slct_meter[i].split('.csv')[0])
 
-    return fig
+    return fig, min, max, marks, value
 
-
+@app.callback(
+    Output('dropdown-container', 'children'),
+    Input('add-filter', 'n_clicks'),
+    State('dropdown-container', 'children'))
+def display_dropdowns(n_clicks, children):
+    print(n_clicks)
+    if n_clicks < 3:
+        new_dropdown = dcc.Dropdown(
+            id={
+                'type': "slct_meter",
+                'index': n_clicks
+            },
+            options=[{'label': i.split('_results.csv')[0], 'value': i} for i in entries],
+            value='BryanDataCenter_results.csv',
+            style={'display': 'inline-block',
+                   'width':'300px'}
+        )
+        children.append(new_dropdown)
+        return children
+    return children
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ src/models/DataLoader.py
@@ -0,0 +1,72 @@
+import os
+import pandas as pd
+import asyncio
+import time
+
+
+class DataLoader:
+    files = None
+    data = None
+    file_names = None
+    DIR = '../data/Analysis/'
+    min = None
+    max = None
+    value = None
+    marks = None
+
+    def __init__(self):
+        self.files = [file for file in os.listdir(self.DIR) if file.endswith('.csv')]
+        # turn filename.csv to filename for all files
+        self.file_names = [os.path.splitext(os.path.basename(x))[0] for x in self.files]
+        self.file_names
+        self.file_names.sort()
+        self.data = {}
+
+    async def start(self):
+        # start loading all data function and forget about it
+        asyncio.ensure_future(self.load_all_data())
+
+    async def load_all_data(self):
+        # this function loads all data in the Analysis directory
+        await asyncio.sleep(1)
+        if self.data == {}:
+            for i in range(len(self.file_names)):
+                try:
+                    self.data[self.file_names[i]] = pd.read_csv(self.DIR + self.files[i], parse_dates=['Datetime'])
+                except FileNotFoundError:
+                    self.data[self.file_names[i]] = pd.DataFrame()
+
+    def load_file(self, file_name) -> pd.DataFrame:
+        # this function loads a single specified file
+        try:
+            if file_name in self.data:
+                if len(self.data[file_name]) != 0:
+                    return self.data[file_name]
+            else:
+                return pd.read_csv(self.DIR + file_name + '.csv', parse_dates=['Datetime'],
+                                   date_parser=lambda col: pd.to_datetime(col, utc=True), index_col='Datetime')
+        except FileNotFoundError:
+            return {file_name: pd.DataFrame([])}
+
+    def get_file_names(self):
+        # this function returns a list of file names
+        return self.file_names
+
+    def x(self, slct_meter):
+        df = self.load_file(slct_meter)
+        min = df.index.year.min()
+        max = df.index.year.max()
+        marks = {i: f'{i}' for i in range(df.index.year.min(), df.index.year.max() + 1, 1)}
+        value = [min, max]
+        return min, max, marks, value
+
+#
+# loader = DataLoader()
+# print(loader.get_file_names())
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(loader.start())
+# print('start')
+# print(loader.load_file('BryanDataCenter_results'))
+# print('second time much faster')
+# print(loader.load_file('BryanDataCenter_results'))
+#
